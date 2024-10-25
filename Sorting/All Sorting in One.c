e071af686838530f6e97ce43a5592f11345d3be8
Rup Roshan Jha <125679654+ruproshanjha@users.noreply.github.com>
2024-10-25 10:41:35 +0530

    Create All Sorting in One.c

---
+++ Sorting/All Sorting in One.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int minIndex = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap arr[i] and arr[minIndex]
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[100], n, choice;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nChoose sorting algorithm:\n1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            bubbleSort(arr, n);
+            printf("Array sorted using Bubble Sort:\n");
+            break;
+        case 2:
+            selectionSort(arr, n);
+            printf("Array sorted using Selection Sort:\n");
+            break;
+        case 3:
+            insertionSort(arr, n);
+            printf("Array sorted using Insertion Sort:\n");
+            break;
+        default:
+            printf("Invalid choice!\n");
+            return 1;
+    }
+
+    printArray(arr, n);
+    return 0;
+}
